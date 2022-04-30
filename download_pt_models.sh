ab3321ff53d09b88104ace93f691f46cd2c0cc6a
Xusheng <jsksxs360@163.com>
2022-04-30 21:37:44 +0800

    update

---
+++ download_pt_models.sh
@@ -0,0 +1,21 @@
+mkdir -p ./PT_MODELS/bert-base-cased/
+wget -P ./PT_MODELS/bert-base-cased/ https://huggingface.co/bert-base-cased/resolve/main/pytorch_model.bin
+wget -P ./PT_MODELS/bert-base-cased/ https://huggingface.co/bert-base-cased/resolve/main/README.md
+wget -P ./PT_MODELS/bert-base-cased/ https://huggingface.co/bert-base-cased/resolve/main/config.json
+wget -P ./PT_MODELS/bert-base-cased/ https://huggingface.co/bert-base-cased/resolve/main/tokenizer.json
+wget -P ./PT_MODELS/bert-base-cased/ https://huggingface.co/bert-base-cased/resolve/main/tokenizer_config.json
+wget -P ./PT_MODELS/bert-base-cased/ https://huggingface.co/bert-base-cased/resolve/main/vocab.txt
+mkdir -p ./PT_MODELS/roberta-base/
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/pytorch_model.bin
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/README.md
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/config.json
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/dict.txt
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/merges.txt
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/tokenizer.json
+wget -P ./PT_MODELS/roberta-base/ https://huggingface.co/roberta-base/resolve/main/vocab.json
+mkdir -p ./PT_MODELS/allenai/longformer-large-4096/
+wget -P ./PT_MODELS/allenai/longformer-large-4096/ https://huggingface.co/allenai/longformer-large-4096/resolve/main/pytorch_model.bin
+wget -P ./PT_MODELS/allenai/longformer-large-4096/ https://huggingface.co/allenai/longformer-large-4096/resolve/main/config.json
+wget -P ./PT_MODELS/allenai/longformer-large-4096/ https://huggingface.co/allenai/longformer-large-4096/resolve/main/merges.txt
+wget -P ./PT_MODELS/allenai/longformer-large-4096/ https://huggingface.co/allenai/longformer-large-4096/resolve/main/tokenizer.json
+wget -P ./PT_MODELS/allenai/longformer-large-4096/ https://huggingface.co/allenai/longformer-large-4096/resolve/main/vocab.json
